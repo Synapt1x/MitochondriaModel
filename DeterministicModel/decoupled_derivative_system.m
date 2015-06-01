a649b7cab46813d936b305e649525ffd46197e86
Synapt1x <chriscadonic@gmail.com>
2015-06-01 14:08:31 -0500

    Changed Derivative system file

--- DeterministicModel/decoupled_derivative_system.m
+++ DeterministicModel/decoupled_derivative_system.m
@@ -8,20 +8,23 @@ function dy = decoupled_derivative_system(t,y,parameters)
     Hp = y(4);
     
     %To decouple the system, complexes I-III activity is instead
-    %approximated by F_0(parameters)
+    %approximated by parameters.f0
     
     %Given this, conservation occurs between NADH and NAD, Succ and Fum,
-    %Q and QH2. Since F_0(parameters) approximates BOTH forward and reverse we get 
+    %Q and QH2. Since parameters.f0 approximates BOTH forward and reverse we get 
     %consumption and production of each component in these pairs as
     %equivalent. Thus the other 1substrates do not change in concentration,
     %and we have their time derivatives equal to 0.
     
-    dy(1) = 2*F_0(parameters) - 4*F_kinetic(Cytcred,O2,Hn,parameters); %dCytcred
-    dy(2) = -F_kinetic(Cytcred,O2,Hn,parameters); %dO2
-    dy(3) = -6*F_0(parameters) - 8*F_kinetic(Cytcred,O2,Hn,parameters) ...
-       + F_Synthase(Hn,Hp,parameters) + F_FCCP(Hn,Hp,parameters); %dHn
-    dy(4) = 8*F_0(parameters) + 4*F_kinetic(Cytcred,O2,Hn,parameters) ...
-        - F_Synthase(Hn,Hp,parameters) - F_FCCP(Hn,Hp,parameters); %dHp
+    %Equations are written out in their entirety here, with alterations
+    %taking place in these equations if the condition changes (i.e.,
+    %baseline vs oligo vs FCCP vs inhibit)
+    dy(1) = 2*parameters.f0 - 4*((parameters.Vmax*s_2)./(parameters.Km.*(1+(parameters.K1))+s_2)).*Hn; %dCytcred
+    dy(2) = -((parameters.Vmax*s_2)./(parameters.Km.*(1+(parameters.K1))+s_2)).*Hn; %dO2
+    dy(3) = -6*parameters.f0 - 8*((parameters.Vmax*s_2)./(parameters.Km.*(1+(parameters.K1))+s_2)).*Hn ...
+       + ((parameters.p1*(Hp/Hn))/((Hp/Hn)+parameters.p2+(parameters.p3/Hn)))*Hp; %dHn
+    dy(4) = 8*parameters.f0 + 4*((parameters.Vmax*s_2)./(parameters.Km.*(1+(parameters.K1))+s_2)).*Hn ...
+        - ((parameters.p1*(Hp/Hn))/((Hp/Hn)+parameters.p2+(parameters.p3/Hn)))*Hp; %dHp
     
     dy=dy'; %correct vector orientation
     
