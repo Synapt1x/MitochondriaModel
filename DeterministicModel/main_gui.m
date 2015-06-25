b304579d97215affc14dfb62d87f5805085aeb9d
Synapt1x <chriscadonic@gmail.com>
2015-06-25 13:11:52 -0500

    Continuing changes with BVP on oxy

--- DeterministicModel/main_gui.m
+++ DeterministicModel/main_gui.m
@@ -152,8 +152,7 @@ guidata(hObject,handles);
 function plot_Callback(hObject, eventdata, handles) %plot button in gui
 
 %plug in the equations into the ode solver
-[t y] = ode23t(@decoupled_derivative_system,handles.parameters.timePoints, ...
-    handles.parameters.initialConditions,[],handles.parameters);
+[t y] = solver(parameters);
 
 %store the values calculated for each variable
 [cytcred o2 Hn Hp] = deal(y(:,1),y(:,2),y(:,3),y(:,4));
--- DeterministicModel/setup.m
+++ DeterministicModel/setup.m
@@ -14,16 +14,16 @@ parameters.K1 = 1495; %bounds: [0.1 1E4]
 parameters.Km = 2096.1; %bounds: [0.1 1E4]
 parameters.p1 = 40.5013; %bounds: [1 1E4]
 parameters.p2 = 1477.4; %bounds: [1 1E4]
-parameters.p3 = 1.415e-5; %bounds: [1E-6 1]
+parameters.p3 = 1.415E-5; %bounds: [1E-6 1]
 parameters.f0 = 1.1797; %bounds: [1 1E4]
-parameters.Dh = 2.4025e-7; %bounds: [1E-6 1]
+parameters.Dh = 2.4025E-7; %bounds: [1E-6 1]
 
 %initial conditions
 parameters.Cytcox = 100;
 parameters.Cytcred = 40;
 parameters.O2 = parameters.realData(1);
-parameters.Hn = 40;
-parameters.Hp = 1;
+parameters.Hn = 100;
+parameters.Hp = 10;
 
 %parameters for defining the IV of the region of interest
 %all the time points for integration from allData
