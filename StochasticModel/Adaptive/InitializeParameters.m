4cfc388aadd22eddf2c80f17315640763dc20458
Ella Thomson <thomsoea@myumanitoba.ca>
2015-07-06 11:10:39 -0500

    Change to mitochondria reactions

--- StochasticModel/Adaptive/InitializeParameters.m
+++ StochasticModel/Adaptive/InitializeParameters.m
@@ -10,12 +10,13 @@ time = 0; % initial time is 0 seconds
 
 times = [0]; % start a vector to hold all times 
 
-X0 = [400 400 800 0 400]; % initial amounts of each reactant stored in a vector
+X0 = [400 400 400 400 400 400 400 400]; % initial amounts of each reactant stored in a vector
 
 X = X0; % used to store amounts of species at all time pts for one sim
 
 num_rx = 4; % the number of different chemical reactions
 
-V = [-1 2 1 1 0; -1 2 -1 0 1; 1 -2 2 1 0; 0 2 1 -1 -1]; % v values for all reactions
+% [cytcred O2 HN HP NADH2 NAD+ Cytcox H20]
+V = [4 0 -12 12 -1 1 -4 0; -4 -1 -8 4 0 0 4 2; 0 0 1 -1 0 0 0 0; 0 0 1 -1 0 0 0 0]; % v values for all reactions
 
-num_species = 5; % the number of species involved in all reactions
+num_species = 8; % the number of species involved in all reactions
