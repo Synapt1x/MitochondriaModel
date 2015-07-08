ec61a273b244a75d5d6a6c6cb64108cdbc58dc56
Synapt1x <chriscadonic@gmail.com>
2015-07-07 19:00:04 -0500

    Added tic toc for benchmark; Changed range of Dh to 0.1:10

--- DeterministicModel/solver.m
+++ DeterministicModel/solver.m
@@ -6,6 +6,7 @@ as a boundary value problem.
 
 %Solve by using ode for each section and passing along the final
 %values as initial values for the next section
+tic
 [t1,y1] = ode45(@baselineSystem, parameters.baselineTimes, ...
     parameters.initialConditions,[],parameters);
 [t2,y2] = ode45(@oligoSystem, parameters.oligoTimes, ...
@@ -14,6 +15,7 @@ as a boundary value problem.
     [y2(end,1),y2(end,2),y2(end,3),y2(end,4)],[],parameters);
 [t4,y4] = ode45(@inhibitSystem, parameters.inhibitTimes, ...
     [y3(end,1),y3(end,2),y3(end,3),y3(end,4)],[],parameters);
+toc
 
 t = [t1;t2;t3;t4];
 y = [y1;y2;y3;y4];
