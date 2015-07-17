2ca13733a0b6979d3aabe3bb174e856c1b851cea
Synapt1x <chriscadonic@gmail.com>
2015-07-17 15:03:03 -0500

    Completed auto analyzeResults; completed load results button in GUI

--- DeterministicModel/analyzeResults.m
+++ DeterministicModel/analyzeResults.m
@@ -9,27 +9,22 @@ results.
 In this function, OptimalSolutions is loaded and then it will be saved to
 the 'Solutions' folder in the format "Date-OptimalSolutions.mat".
 This structure will then be analyzed in the code below to look for the
-global best, which will be stored in the global variable myResults. 
+global best, which will be stored in the variable result. 
 %}
 
 %% Initialize vars
 
-global myResults; %global variable for storing the best parameter set
-global bestFit; %global variable for storing the best fit
-
-[X,F(1),F(2)] = deal(inf); %initialize storage variables
+[myResults,bestFit(1),bestFit(2)] = deal(inf); %initialize storage variables
 
 %% Loop over OptimalSolutions to find Best
 
 % To loop over entire optimal set:
 for n=1:size(OptimalSolutions.X, 2) % Number of columns = number of solutions.
-    if OptimalSolutions.F(1,n)<F(1) && OptimalSolutions.F(2,n)<F(2)
-        X=OptimalSolutions.X(:,n);
-        F=OptimalSolutions.F(:,n);
+    if OptimalSolutions.F(1,n)<bestFit(1) && OptimalSolutions.F(2,n)<bestFit(2)
+        myResults=OptimalSolutions.X(:,n);
+        bestFit=OptimalSolutions.F(:,n);
     end
 end
-myResults = X;
-bestFit = F;
 
 %% Save files to Solutions folder
 
