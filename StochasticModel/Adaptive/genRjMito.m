79948a7154c5b0c5916ad0ba484b37380546c20f
Ella Thomson <thomsoea@myumanitoba.ca>
2015-07-30 11:51:56 -0500

    Change c4 back to 10^-4
    10^-3 increased the slope for rotenone and antimycin a

--- StochasticModel/Adaptive/genRjMito.m
+++ StochasticModel/Adaptive/genRjMito.m
@@ -33,7 +33,7 @@ p4=0.1885;
 c1=10^-5;
 c2=10^-2;
 c3=10^-20;
-c4=10^-3.5;
+c4=10^-4;
 
 % find ajs for each reaction and store in a vector. These need to be
 % changes based on the reactions defined in initializeParameters. Each aj
