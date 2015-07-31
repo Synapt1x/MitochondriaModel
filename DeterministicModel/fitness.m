0196f8c1ba2727d09734f9713620e41d1c082f2e
Synapt1x <chriscadonic@gmail.com>
2015-07-31 11:09:49 -0500

    Add Ferret run after gen 10 with F~=43
    Committing after running Ferret for over a day, with gen producing a fit at approximately 43.

--- DeterministicModel/fitness.m
+++ DeterministicModel/fitness.m
@@ -6,23 +6,23 @@ f=fields(X);
 f(strcmpi(f,'info'))=[];
 
 for n=length(X):-1:1
-    if isAbortEval(extPar.status)
-        F=[];
-        break
-    end
-    
-    for i=1:length(f)
-        parameters.(f{i})=X(n).(f{i});
-    end
-    
-    %call ode to solve the system of equations for this solver
-    [t, y] = solver(parameters);
-    
-    %for fitting O2
-    evaluations = y(:,2); %evaluated data for o2
-    realo2Data = parameters.realo2Data; %use actual o2 data
-    
-    %evaluate using a least-squares
-    F(1,n) = sum((realo2Data-evaluations).^2)/numel(realo2Data);
-    
+        if isAbortEval(extPar.status)
+                F=[];
+                break
+        end
+        
+        for i=1:length(f)
+                parameters.(f{i})=X(n).(f{i});
+        end
+        
+        %call ode to solve the system of equations for this solver
+        [t, y] = solver(parameters);
+        
+        %for fitting O2
+        evaluations = y(:,2); %evaluated data for o2
+        realo2Data = parameters.realo2Data; %use actual o2 data
+        
+        %evaluate using a least-squares
+        F(1,n) = sum((realo2Data-evaluations).^2)/numel(realo2Data);
+        
 end
