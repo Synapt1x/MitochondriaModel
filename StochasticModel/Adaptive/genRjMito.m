96bfa42b81f69c5183161ceae31ffe337fc564db
Ella Thomson <thomsoea@myumanitoba.ca>
2015-08-04 13:12:31 -0500

    Increase c4

--- StochasticModel/Adaptive/genRjMito.m
+++ StochasticModel/Adaptive/genRjMito.m
@@ -34,7 +34,7 @@ p4=0.1885;
 c1=10^-6;
 c2=1.5*(10^-2);
 c3=10^-30;
-c4=10^-10;
+c4=10^-8;
 
 % find ajs for each reaction and store in a vector. These need to be
 % changes based on the reactions defined in initializeParameters. Each aj
