22ca856f119cacc2bbcf745b542598c3f58cb1c3
Ella Thomson <thomsoea@myumanitoba.ca>
2015-08-05 14:09:54 -0500

    Prevent negative species 7 population

--- StochasticModel/Adaptive/evalCrit.m
+++ StochasticModel/Adaptive/evalCrit.m
@@ -5,7 +5,7 @@ function nc = evalCrit(X0)
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 averageStart = sum(X0) / length(X0);
-nc = 0.1.* averageStart;  % crticial amount is 10% of starting amount
+nc = 0.01.* averageStart;  % crticial amount is 10% of starting amount
 
 % future testing will keep critical percentage at 1. Lower percentages
 % extended the time required for the simulation 
