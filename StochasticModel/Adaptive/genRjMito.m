ca426f6eabbfba1463d8fcf2e8f932029d104c60
Ella Thomson <thomsoea@myumanitoba.ca>
2015-08-12 14:36:44 -0500

    Save image files

--- StochasticModel/Adaptive/genRjMito.m
+++ StochasticModel/Adaptive/genRjMito.m
@@ -31,8 +31,8 @@ f0= 2*6.02*(10^14)*95.3875;
 p4=0.1885;
 
 % constants list (cjs) modified from original best set
-c1=1*(10^-4); % Best value is 5*(10^-4)try to change this parameter up and down 8.5 8.834 *(10^-5). Increases in c1 seem to be lowering species 1 cncentration to negative ranges 
-c2=3*(10^-2); % stick with 2.65 *(10^-2)
+c1=8.83*(10^-5); % Best value is 5*(10^-4)try to change this parameter up and down 8.5 8.834 *(10^-5). Increases in c1 seem to be lowering species 1 cncentration to negative ranges 
+c2=2.64*(10^-2); % stick with 2.65 *(10^-2)
 c3=10^-30; % stick with 10^-30. Higher values slow down the simulation 
 c4=8*(10^-8); % decrease this parameter  2*(10^-8)
 
