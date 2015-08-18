6bb9053dbbf40d9bdc8927e40117206667baa9b9
Ella Thomson <thomsoea@myumanitoba.ca>
2015-08-18 15:44:46 -0500

    Halve constants for V

--- StochasticModel/Adaptive/SSATestAdaptiveV2.m
+++ StochasticModel/Adaptive/SSATestAdaptiveV2.m
@@ -9,7 +9,7 @@ times. The program plots the quantities of all three substances vs time.
 It produces two plots; one figure with each substance on its own plot,
 and one figure with all three substances on the same plot.
 %}
-
+ 
 % user chooses how many simulations to run
 num_sims = 1;
 
