8f0358e19a119f8a37941a4d72422121a66cc9f1
Synapt1x <chriscadonic@gmail.com>
2015-10-06 12:34:01 -0500

    Re-add sensitivity analysis .m file
    Re-added the .m file for sensitivity analysis. This will then be used in
    conjunction with the lhsdesign function in matlab to carry out latin
    hypercube sampling for the sensitivity analysis of the model.

---
+++ DeterministicModel/SensitivityAnalysis/sensitivityAnalysis.m
@@ -0,0 +1,36 @@
+function modelJacobi = sensitivityAnalysis()
+%{
+Created by: Chris Cadonic
+========================================
+This function houses code for using symbolic variables to represent
+functions in my deterministic model. This function will be essential
+to conducting sensitivity analysis for my masters project by finding
+the Jacobian of the set of equations in my model. This will be done
+using the symbolic functions for each equation in my model as
+carried out below.
+This will be carrying out sensitivty analysis for the baseline system.
+%}
+
+%Initialize the symbolic variables in the model; vars, params and t
+syms r o omega rho f0vmax f0K vmax K1 Km p1 p2 p3 p4 t cytcdiff;
+
+%define cytcdiff
+cytcdiff = 100.1 - r;
+
+%Define each of the baseline equations from the mito model
+f1 = 2*((f0vmax*(cytcdiff))/(f0K+(cytcdiff))) ...
+        *(omega./rho) - 2*((vmax*o)/(Km*(1 ...
+        +(K1/r))+o))*(omega/rho); %dr
+f2 = -0.5*((vmax*o)/(Km*(1+(K1/r))+o)) ...
+      *(omega/rho); %do
+f3 = -6*((f0vmax*(cytcdiff))/(f0K+(cytcdiff))) ...
+        *(omega./rho) - 2*((vmax*o)/(Km*(1 ...
+        +(K1/r))+o))*(omega/rho) + ((p1 ...
+        *(rho/omega))/((rho/omega)+p2 + (p3/omega)))*rho; %domega
+
+%define arrays containing all funcs and all params
+funcs = [f1,f2,f3];
+params = [f0vmax,f0K,vmax,K1,Km,p1,p2,p3,t];
+
+%call jacobian to calculate the jacobian function to calc all derivs
+modelJacobi = jacobian(funcs,params);
