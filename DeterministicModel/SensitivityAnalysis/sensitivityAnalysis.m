a7ef24b9a437661982e14d7896b582828f682441
Synapt1x <chriscadonic@gmail.com>
2015-11-05 13:15:10 -0600

    Add work on sensitivity analysis

--- DeterministicModel/SensitivityAnalysis/sensitivityAnalysis.m
+++ DeterministicModel/SensitivityAnalysis/sensitivityAnalysis.m
@@ -1,4 +1,4 @@
-function [equations,lhs, output] = sensitivityAnalysis()
+function sensitivityOutput = sensitivityAnalysis()
 %{
 Created by: Chris Cadonic
 ========================================
@@ -14,41 +14,64 @@ This will be carrying out sensitivty analysis for the baseline system.
 %Initialize the symbolic variables in the model; vars, params and t
 syms r o omega rho f0vmax f0K vmax K1 Km p1 p2 p3 p4 t cytcdiff;
 [equations, output] = deal({});
+r = 0.1;
+o = 171.0549;
+omega = 100;
+rho = 0.0398;
+
+%Initialize output parameters
+sensitivityOutput.equations = [];
+[sensitivityOutput.outputLabels, output, ...
+      sensitivityOutput.outputVals] = deal({});
 
 %define cytcdiff
 cytcdiff = 100.1 - r;
 
 %Define each of the baseline equations from the mito model
-f1 = 2*((f0vmax*(cytcdiff))/(f0K+(cytcdiff))) ...
+dr = 2*((f0vmax*(cytcdiff))/(f0K+(cytcdiff))) ...
         *(omega./rho) - 2*((vmax*o)/(Km*(1 ...
         +(K1/r))+o))*(omega/rho); %dr
-f2 = -0.5*((vmax*o)/(Km*(1+(K1/r))+o)) ...
+do = -0.5*((vmax*o)/(Km*(1+(K1/r))+o)) ...
       *(omega/rho); %do
-f3 = -6*((f0vmax*(cytcdiff))/(f0K+(cytcdiff))) ...
+domega = -6*((f0vmax*(cytcdiff))/(f0K+(cytcdiff))) ...
         *(omega./rho) - 2*((vmax*o)/(Km*(1 ...
         +(K1/r))+o))*(omega/rho) + ((p1 ...
         *(rho/omega))/((rho/omega)+p2 + (p3/omega)))*rho; %domega
 
 %define arrays containing all funcs and all params
-funcs = [f1,f2,f3];
+funcs = [dr,do,domega];
 params = [f0vmax,f0K,vmax,K1,Km,p1,p2,p3,t];
 
 %call jacobian to calculate the jacobian function to calc all derivs
 jacobianMatrix = jacobian(funcs,params);
 
 % create the sampling pool using latin hypercube sampling
-lhs = lhsdesign(1E4,9);
+lhs = lhsdesign(18,9);
 
+disp('Generating equations using latin hypercube sampling...');
+
+% loop over and apply lhs to each equation
 for eq =1:numel(jacobianMatrix)
-      tic
+      % each parameter sensitivity is stored in jacobianMatrix
       equations{eq} = jacobianMatrix(eq); %store each equation
       
       lhsCell = num2cell(lhs); %convert to cell matrix
       for i=1:size(lhs,1)
             [f0vmax, f0K, vmax, K1, Km, p1, p2, p3, t] = deal(lhsCell{i,:});
-      
+
             output{i,eq} = subs(jacobianMatrix(eq));
+            output{i,eq} = double(output{i,eq});
       end
-      toc
 end
 
+% create label matrix
+sensitivityOutput.outputLabels = {'dr/df0vmax','do/df0vmax', ...
+      'domega/df0vmax','dr/df0K','do/df0K','domega/df0K', 'dr/dvmax', ...
+      'do/dvmax','domega/dvmax', 'dr/dK1','do/dK1','domega/dK1','dr/dKm', ...
+      'do/dKm','domega/dKm', 'dr/dp1','do/dp1','domega/dp1', 'dr/dp2', ...
+      'do/dp2','domega/dp2', 'dr/dp3','do/dp3','domega/dp3', 'dr/dt', ...
+      'do/dt','domega/dt'};
+
+% convert from symbolic notation and store in structure
+sensitivityOutput.equations = vpa(equations);
+sensitivityOutput.outputVals = cell2mat(output);
