5c06f1e80090f856656cba4d840cd38959b001e2
Synapt1x <chriscadonic@gmail.com>
2016-04-12 17:54:39 -0500

    Ready to optimize strategy I

--- DeterministicModel/FerretSetup.m
+++ DeterministicModel/FerretSetup.m
@@ -50,11 +50,11 @@ par.X.cytcred.range=[1E-2,1E2];
 %
 par.X.cytcox.range=[1E-2,1E2];
 %
-%par.X.oxygen.range=[1E-2,1E4];
+par.X.oxygen.range=[1E-2,1E4];
 %
 par.X.omega.range=[1E-3,1E3];
 %
-%par.X.rho.range=[1E-3,1E3];
+par.X.rho.range=[1E-3,1E3];
 %
 % ====================================
 % Parallel Computing
