25fdf8081e0d4acc3610d81a98986ad65f21ec5a
Synapt1x <chriscadonic@gmail.com>
2016-04-15 11:52:53 -0500

    Update files to be run in Ferret

--- DeterministicModel/fitness.m
+++ DeterministicModel/fitness.m
@@ -16,15 +16,21 @@ for n=length(X):-1:1
                 params.(f{i})=X(n).(f{i});
         end
         
+        warning off
         %call ode to solve the system of equations for this solver
         [t, y] = solver(parameters,params);
+        warning on
         
         %for fitting O2
         evaluations = y(:,2); %evaluated data for o2
         realo2Data = parameters.realo2Data; %use actual o2 data
         
-        %evaluate using a least-squares
-        F(1,n) = sum((realo2Data-evaluations).^2)/numel(realo2Data);
-        pause(0.001);
+        try
+            %evaluate using a least-squares
+            F(1,n) = sum((realo2Data-evaluations).^2)/numel(realo2Data);
+            pause(0.001);
+        catch
+            F(1,n) = 1E6;
+        end
         
 end
