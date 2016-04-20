de86419bcc15632fa42f7c6105a8951210450a53
Synapt1x <chriscadonic@gmail.com>
2016-04-20 12:35:58 -0500

    Add optimizing cyt results to baseline

--- DeterministicModel/solver.m
+++ DeterministicModel/solver.m
@@ -16,11 +16,11 @@ tic
     [parameters.Cytcred,parameters.O2,parameters.Hn, ...
     parameters.Hp],options,params);
 [t2,y2] = ode45(@oligoSystem, parameters.oligoTimes, ...
-    [y1(end,1),y1(end,2),y1(end,3),y1(end,4)],options,params)
+    [y1(end,1),y1(end,2),y1(end,3),y1(end,4)],options,params);
 [t3,y3] = ode45(@fccpSystem, parameters.fccpTimes, ...
-    [y2(end,1),y2(end,2),y2(end,3),y2(end,4)],options,params)
+    [y2(end,1),y2(end,2),y2(end,3),y2(end,4)],options,params);
 [t4,y4] = ode45(@inhibitSystem, parameters.inhibitTimes, ...
-    [y3(end,1),y3(end,2),y3(end,3),y3(end,4)],options,params)
+    [y3(end,1),y3(end,2),y3(end,3),y3(end,4)],options,params);
 toc
 
 t = [t1;t2;t3;t4];
