66f63bc1a444f3ba49647e17be34ff0ae940b85e
Synapt1x <chriscadonic@gmail.com>
2017-05-24 14:48:08 -0500

    Ready to fit; need to test if fitting pressure

--- DeterministicModel/FerretSetup.m
+++ DeterministicModel/FerretSetup.m
@@ -50,7 +50,7 @@ par.X.cytcred.range=[1E-3,1E4];
 %
 par.X.cytcox.range=[1E-3,1E4];
 %
-par.X.alpha.range=[1E-9,1E2];
+par.X.alpha.range=[1E-9,1];
 %
 % ====================================
 % Parallel Computing
