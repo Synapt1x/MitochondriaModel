b9b09da59dfe8291c522f70cdc942fd10bdf6cf7
Synapt1x <chriscadonic@gmail.com>
2018-07-31 22:27:24 -0500

    Start working on multi sensitivity analysis for time evolution prcc

--- DeterministicModel/SensitivityAnalysis/PRCCsensitivityanalysis/sensitivityAnalysis.m
+++ DeterministicModel/SensitivityAnalysis/PRCCsensitivityanalysis/sensitivityAnalysis.m
@@ -1,104 +1,154 @@
 function sensitivityAnalysis()
-%{
-Created by: Chris Cadonic
-========================================
-This function houses code for using symbolic variables to represent
-functions in my deterministic model. This function will be essential
-to conducting sensitivity analysis for my mitochondrial model by finding
-the Jacobian of the set of equations in my model. This will be done
-using the symbolic functions for each equation in my model as
-carried out below.
-This will be carrying out sensitivty analysis for the baseline system.
-%}
+    %{
+    Created by: Chris Cadonic
+    ========================================
+    This function houses code for using symbolic variables to represent
+    functions in my deterministic model. This function will be essential
+    to conducting sensitivity analysis for my mitochondrial model by finding
+    the Jacobian of the set of equations in my model. This will be done
+    using the symbolic functions for each equation in my model as
+    carried out below.
+    This will be carrying out sensitivty analysis for the baseline system.
+    %}
 
-%% Create LHS sampling and Sensitivity Coefficients
+    %% Create LHS sampling and Sensitivity Coefficients
 
-%%%%%%%%%%%%%%%%%%%%%%%% define parameters for run %%%%%%%%%%%%%%%%%%%%%%%%
+    %%%%%%%%%%%%%%%%%%%%%%%% define parameters for run %%%%%%%%%%%%%%%%%%%%%%%%
 
-num_sims = 40;
-display_interval = num_sims / 4;
-max_t = 1E3;
-% lower bounds for params
-lb = [1E5, 1, ... %f0
-    1E6, 1E5, 1E5, ... %fIV
-    1E5, 5E5, 5E-4, ... %fV
-    1E3, 1E3, 0.05, 1E-3, 1E-3, 1E-3, 1E-3, 0.05];
-    % last row: r0, ox0, leak, amp1-4, attenuate
-% upper bounds for params
-ub = [1E6, 50, ... %f0
-    1E7, 1E6, 1E6, ... %fIV
-    1E6, 5E6, 5E-3, ... %fV
-    5E3, 5E3, 1.0, 5E-3, 5E-3, 5E-3, 5E-3, 1.0];
-    % last row: r0, ox0, leak, amp1-4, attenuate
-% set parameters for time evolution
+    % universal and single-run parameters
+    num_sims = 40;
+    display_interval = num_sims / 4;
+    max_t = 1E3;
+    % lower bounds for params
+    lb = [1E5, 1, ... %f0
+        1E6, 1E5, 1E5, ... %fIV
+        1E5, 5E5, 5E-4, ... %fV
+        1E3, 1E3, 0.05, 1E-3, 1E-3, 1E-3, 1E-3, 0.05];
+        % last row: r0, ox0, leak, amp1-4, attenuate
+    % upper bounds for params
+    ub = [1E6, 50, ... %f0
+        1E7, 1E6, 1E6, ... %fIV
+        1E6, 5E6, 5E-3, ... %fV
+        5E3, 5E3, 1.0, 5E-3, 5E-3, 5E-3, 5E-3, 1.0];
+        % last row: r0, ox0, leak, amp1-4, attenuate
+        
+    % set parameters for time evolution
+    num_time_samples = 100;
+    num_multi_sims = 40;
 
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    % clear cmd history for clarity
+    clc
 
-% clear cmd history for clarity
-clc
+    % store the current directory and move up to acquire model information
+    curdir = fileparts(which(mfilename));
+    cd(['..',filesep,'..']);
 
-% store the current directory and move up to acquire model information
-curdir = fileparts(which(mfilename));
-cd(['..',filesep,'..']);
+    % acquire initial setup data for the model
+    [all_params, data, ~] = setup;
+    params = all_params.ctrlParams;
 
-% acquire initial setup data for the model
-[all_params, data, ~] = setup;
-params = all_params.ctrlParams;
+    cd(curdir);
 
-cd(curdir);
+    %Initialize the symbolic variables in the model; vars, params and t
+    parameters = {'f0_Vmax', 'f0_Km', 'fIV_Vmax', 'fIV_K', 'fIV_Km', ...
+        'fV_Vmax', 'fV_K', 'fV_Km', 'r0', 'ox0', 'p_leak', 'amp_1', ...
+        'amp_2', 'amp_3', 'amp_4', 'r_attenuate'};
+    initial_params = [params.cytcred, params.oxygen, params.omega, params.rho];
+    num_params = numel(parameters);
 
-%Initialize the symbolic variables in the model; vars, params and t
-parameters = {'f0_Vmax', 'f0_Km', 'fIV_Vmax', 'fIV_K', 'fIV_Km', ...
-    'fV_Vmax', 'fV_K', 'fV_Km', 'r0', 'ox0', 'p_leak', 'amp_1', ...
-    'amp_2', 'amp_3', 'amp_4', 'r_attenuate'};
-initial_params = [params.cytcred, params.oxygen, params.omega, params.rho];
-num_params = numel(parameters);
+    %Initialize output parameters
+    sensitivityOutput.outputLabels = {};
+    sensitivityOutput.finalVals = zeros(num_sims, 1);
+    sensitivityOutput.prcc = [];
+    sensitivityOutput.sensitivity = struct();
 
-%Initialize output parameters
-sensitivityOutput.outputLabels = {};
-sensitivityOutput.finalVals = zeros(num_sims, 1);
-sensitivityOutput.prcc = [];
-sensitivityOutput.sensitivity = struct();
+    %% Run single test ================================================ %%
 
-%% Run single test
+    % create the sampling pool using latin hypercube sampling
+    lhsRaw = lhsdesign(num_sims, numel(parameters));
+    lhs = bsxfun(@plus, lb, bsxfun(@times, lhsRaw, (ub-lb))); % rescale
 
-% create the sampling pool using latin hypercube sampling
-lhsRaw = lhsdesign(num_sims, numel(parameters));
-lhs = bsxfun(@plus, lb, bsxfun(@times, lhsRaw, (ub-lb))); % rescale
+    %% Generate output matrix from simulations
 
-%% Generate output matrix from simulations
+    disp('Simulating outputs from LHS sampling...');
 
-disp('Simulating outputs from LHS sampling...');
+    % calculate all output vals using simulations
+    sensitivityOutput.finalVals, all_y = calc_output(params, lhs, data, ...
+        initial_params, display_interval);
 
-% calculate all output vals using simulations
-sensitivityOutput.finalVals = calc_output(params, lhs, data, ...
-    initial_params, display_interval);
+    % firstly, remove all NaN result rows
+    [remove_rows, ~] = find(isnan(sensitivityOutput.finalVals));
+    sensitivityOutput.finalVals(remove_rows) = [];
+    lhs(remove_rows, :) = [];
+    n = numel(lhs(:, 1));
 
-% firstly, remove all NaN result rows
-[remove_rows, ~] = find(isnan(sensitivityOutput.finalVals));
-sensitivityOutput.finalVals(remove_rows) = [];
-lhs(remove_rows, :) = [];
-n = numel(lhs(:, 1));
+    % calculate rank order matrices
+    [~, rank_lhs] = sort(lhs, 'ascend');
+    [~, rank_out] = sort(sensitivityOutput.finalVals, 'descend');
 
-% calculate rank order matrices
-[~, rank_lhs] = sort(lhs, 'ascend');
-[~, rank_out] = sort(sensitivityOutput.finalVals, 'descend');
+    sensitivityOutput.prcc = calc_prcc(rank_out, rank_lhs);
 
-sensitivityOutput.prcc = calc_prcc(rank_out, rank_lhs);
+    % save prcc and sensitivity value to output
+    for p=1:num_params
+        param_name = parameters{p};
+        val = sensitivityOutput.prcc(p);
+        sensitivityOutput.sensitivity.(param_name) = val;
+        disp([param_name, ' sensitivity: ', num2str(val)]);
+    end
 
-% save prcc and sensitivity value to output
-for p=1:num_params
-    param_name = parameters{p};
-    val = sensitivityOutput.prcc(p);
-    sensitivityOutput.sensitivity.(param_name) = val;
-    disp([param_name, ' sensitivity: ', num2str(val)]);
-end
+    %% Multiple tests at key time points ========================== %%
 
-save sensitivityOutput.mat sensitivityOutput
+    %Initialize output parameters
+    sensitivityOutput.time_prcc = [];
+    
+    % assemble the time points
+    all_t = [data.baseline_times; data.oligo_fccp_times; ...
+        data.inhibit_times];
+    num_t = numel(all_t);
+    num_time_samples = min([num_time_samples, num_t]);
+    sample_times_idx = linspace(1, num_t, num_time_samples);
+    sample_times = all_t(sample_times_idx);
+    % extra the comparison data at these time points
+    compare_data = data.CtrlO2(sample_times);
+    
+    for t_i=1:num_time_samples
 
-%% run multiple tests at key time points
+        % create the sampling pool using latin hypercube sampling
+        lhsRaw = lhsdesign(num_sims, numel(parameters));
+        lhs = bsxfun(@plus, lb, bsxfun(@times, lhsRaw, (ub-lb))); % rescale
 
+        %% Generate output matrix from simulations
+
+        disp('Simulating outputs from LHS sampling...');
+
+        % calculate all output vals using simulations
+        sensitivityOutput.finalVals = calc_output(params, lhs, data, ...
+            initial_params, display_interval);
+
+        % firstly, remove all NaN result rows
+        [remove_rows, ~] = find(isnan(sensitivityOutput.finalVals));
+        sensitivityOutput.finalVals(remove_rows) = [];
+        lhs(remove_rows, :) = [];
+        n = numel(lhs(:, 1));
+
+        % calculate rank order matrices
+        [~, rank_lhs] = sort(lhs, 'ascend');
+        [~, rank_out] = sort(sensitivityOutput.finalVals, 'descend');
+
+        sensitivityOutput.prcc = calc_prcc(rank_out, rank_lhs);
+
+        % save prcc and sensitivity value to output
+        for p=1:num_params
+            param_name = parameters{p};
+            val = sensitivityOutput.prcc(p);
+            sensitivityOutput.sensitivity.(param_name) = val;
+            disp([param_name, ' sensitivity: ', num2str(val)]);
+        end
+    end
+
+    save sensitivityOutput.mat sensitivityOutput;
 
 end
 
@@ -116,17 +166,13 @@ function final_vals = calc_output(params, param_lhs, data, ...
     
     % initialize final values vector for output
     final_vals = zeros(num_sims, 1);
+    all_y = [];
     
     warning off;
     progress = 25;
     
     % set some options for ode23t
     options = odeset('NonNegative',[1,2,3,4]);  % settings for ode
-
-    %Setup a fallback set of times in cases of unsolvable parameter sets
-    t_fallback = [data.baseline_times; data.oligo_fccp_times; ...
-        data.inhibit_times];
-    num_times = numel(t_fallback);
 
     for simnum=1:num_sims
         % little display for progress through simulations
@@ -164,6 +210,7 @@ function final_vals = calc_output(params, param_lhs, data, ...
 
             final_vals(simnum) = sum((realo2Data-evaluations).^2)...
                 /numel(realo2Data);
+            all_y = [all_y, y(:, 2)];
         end
     end
     
